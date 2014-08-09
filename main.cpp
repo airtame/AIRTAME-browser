84604f7f7e92c062467a955c66938b2729f8915a
Attila Sukosd <as@airtame.com>
2014-08-09 10:09:15 +0200

    D/browser: Added commandline parsing

--- main.cpp
+++ main.cpp
@@ -1,16 +1,34 @@
-//! [main program]
 #include <unistd.h>
 #include <QtWidgets>
 #include "window.h"
 
 int main(int argc, char *argv[])
 {
+    /* Initialize the application */
     QApplication app(argc, argv);
+    QCoreApplication::setApplicationName("airtame-browser");
+    QCoreApplication::setApplicationVersion("1.0");
+
+    /* Parse the cmdline arguments */
+    QCommandLineParser parser;
+    parser.setApplicationDescription("AIRTAME Browser");
+    parser.addHelpOption();
+    parser.addVersionOption();
+    QCommandLineOption fullscreenOption("f",QCoreApplication::translate("main", "Set browser to be full screen"));
+    parser.addOption(fullscreenOption);
+
+    parser.process(app);
+    bool fullscreen = parser.isSet(fullscreenOption);
+
+    /* Setup the window */
     Window window;
     window.setWindowFlags(Qt::WindowStaysOnTopHint | Qt::FramelessWindowHint);
     
     window.setUrl(QUrl(argv[1]));
-    window.showFullScreen();
+    if (fullscreen) {
+        window.showFullScreen();
+    } else {
+        window.show();
+    }
     return app.exec();
 }
-//! [main program]
