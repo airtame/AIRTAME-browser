c5bbf51bb4e73bd5d9837155651f83eaf956c020
Florin Maticu <florin@airtame.com>
2015-10-23 15:46:43 +0200

    F/browser: Added support for zmq library + refactoring
    Change-Id: If1a2a2612f719a31927370f8e58c1a583e05c0cd

--- browser.h
+++ browser.h
@@ -5,6 +5,8 @@
 #include <QApplication>
 #include <QtNetwork/QLocalServer>
 #include <QWidget>
+#include <QThread>
+#include "zmqserver.h"
 #include "window.h"
 
 class BrowserApplication : public QApplication {
@@ -13,22 +15,29 @@ class BrowserApplication : public QApplication {
          BrowserApplication(int &argc, char **argv);
 
          bool isRunning();                // Are there instances running
-         Window *w;                        // The MainWindow pointer
+         Window *mainWindow;             // The MainWindow pointer
 
      private slots:
          // A trigger new connections
          void _newLocalConnection();
          // Read from socket
          void _readSocket();
+         void _processDataFromZmqServer(QString data);
 
      private:
          // Create the server
          void _newLocalServer();
          // Activates the window
          void _activateWindow();
+        //starts the zmq server
+         void _activateZmqServer();
 
          QLocalServer *_localServer;     // The local socket Server
+         QThread* thread;
+         ZMQServer* zmqServer;
          QString _serverName;            // Service name
+
+
 };
 
 #endif // BROWSER_H
